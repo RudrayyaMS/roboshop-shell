679d685897c7e141cfe5c541ea8c0412015c44c6
Rudra <Rudrayyaswamims@gmail.com>
2023-04-27 23:02:35 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -12,6 +12,8 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[35mCopying the Nginx config for roboshop\e[0m"
+pwd
+ls -l
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[35mEnabling Nginx\e[0m"
