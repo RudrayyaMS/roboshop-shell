ef438f3ecc7fa3f7f801d4a0e396ed4ebb247915
Rudra <Rudrayyaswamims@gmail.com>
2023-04-30 19:55:59 +0530

    updating roboshop-shell

--- common.sh
+++ common.sh
@@ -14,3 +14,71 @@ else
   exit 1
 fi
 }
+
+nodejs() {
+  print_head "Configure nodejs Repo"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
+
+  print_head "Install nodejs"
+  yum install nodejs -y &>>${log_file}
+  status_check $?
+
+  print_head "Create Roboshop User"
+  id roboshop &>>${log_file}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${log_file}
+  fi
+  status_check $?
+
+  print_head "Create Application Directory"
+  if [ ! -d /app ]; then
+    mkdir /app &>>${log_file}
+  fi
+  status_check $?
+
+  print_head "Delete old content"
+  rm -rf /app/* &>>${log_file}
+  status_check $?
+
+  print_head "Downloading App Content"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+  cd /app
+  status_check $?
+
+  print_head "Extracting App Content"
+  unzip /tmp/${component}.zip &>>${log_file}
+  status_check $?
+
+  print_head "Installing Nodejs Dependencies"
+  npm install &>>${log_file}
+  status_check $?
+
+  print_head "Copy Systmed Service File"
+  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  status_check $?
+
+  print_head "Reload Systemd"
+  systemctl daemon-reload &>>${log_file}
+  status_check $?
+
+  print_head "Enable ${component} Service"
+  systemctl enable ${component} &>>${log_file}
+  status_check $?
+
+  print_head "Start ${component} Service"
+  systemctl restart ${component} &>>${log_file}
+  status_check $?
+
+  print_head "Copy MongoDB Repo File"
+  cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+  status_check $?
+
+  print_head "Install Mongo Client"
+  yum install mongodb-org-shell -y &>>${log_file}
+  status_check $?
+
+  print_head "Load Schema"
+  mongo --host mongodb.devopsm71.online </app/schema/${component}.js &>>${log_file}
+  status_check $?
+}
